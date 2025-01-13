2f3b882384c19ee02174ca87de9574d7b6c19e87
hezaki <leronpro10@gmail.com>
2025-01-13 21:09:43 +0400

    Mon Jan 13 09:09:43 PM +04 2025

--- hosts/ktsrgi/default.nix
+++ hosts/ktsrgi/default.nix
@@ -2,26 +2,25 @@
 {
   system.stateVersion = "24.05";
   enviroment.packages = with pkgs; [
-      fd
-      duf
-      tgpt
-      ncdu
-      cava
-      lutgen
-      joshuto
-      ripgrep
-      btop
-      onefetch
-      fastfetch
-      home-manager
-    ];
-  };
+    fd
+    duf
+    tgpt
+    ncdu
+    cava
+    lutgen
+    joshuto
+    ripgrep
+    btop
+    onefetch
+    fastfetch
+    home-manager
+  ];
 
   imports = [
     ./programs/fastfetch
-    ../hezaki/programs/terminal/tmux
-    ../hezaki/programs/terminal/shell/zsh
-    ../hezaki/programs/terminal/shell/translate-shell
-    ../hezaki/programs/terminal/editors/neovim
+    ../../hezaki/programs/terminal/tmux
+    ../../hezaki/programs/terminal/shell/zsh
+    ../../hezaki/programs/terminal/shell/translate-shell
+    ../../hezaki/programs/terminal/editors/neovim
   ];
 }
