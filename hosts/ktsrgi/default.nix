10576292ada666d998fbf31b7615bfd98d8c72bc
hezaki <leronpro10@gmail.com>
2025-01-13 21:17:08 +0400

    Mon Jan 13 09:17:08 PM +04 2025

--- hosts/ktsrgi/default.nix
+++ hosts/ktsrgi/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   system.stateVersion = "24.05";
-  enviroment.packages = with pkgs; [
+  environment.packages = with pkgs; [
     fd
     duf
     tgpt
@@ -16,11 +16,14 @@
     home-manager
   ];
 
-  imports = [
-    ./programs/fastfetch
-    ../../home/hezaki/programs/terminal/tmux
-    ../../home/hezaki/programs/terminal/shell/zsh
-    ../../home/hezaki/programs/terminal/shell/translate-shell
-    ../../home/hezaki/programs/terminal/editors/neovim
-  ];
+  home-manager.config = {
+    home.stateVersion = "24.05";
+    imports = [
+      ./programs/fastfetch
+      ../../home/hezaki/programs/terminal/tmux
+      ../../home/hezaki/programs/terminal/shell/zsh
+      ../../home/hezaki/programs/terminal/shell/translate-shell
+      ../../home/hezaki/programs/terminal/editors/neovim
+    ];
+  };
 }
